4eaafb5599edea63d40086f00b1019c265641bc6
plamank <paolomanca@gmail.com>
2015-01-11 12:34:20 +0100

    estrazione di discountFactor

--- src/org/acme/refactoring/replacetempquery/ReplaceTempWithQuery.java
+++ src/org/acme/refactoring/replacetempquery/ReplaceTempWithQuery.java
@@ -3,7 +3,7 @@ package org.acme.refactoring.replacetempquery;
 /**
  * Martin Fowler, "Refactoring", ch. 6 Composing Methods
  * Replace Temp With Query
- * Versione 0.x, rimuovere temp dichiarazione
+ * Versione 0.x, estrazione di discountFactor
  */
 public class ReplaceTempWithQuery {
 	
@@ -11,18 +11,18 @@ public class ReplaceTempWithQuery {
 	private int _itemPrice = 500;
 
 	public double getPrice() {
-		final double discountFactor;
-		if (basePrice() > 1000) {
-			discountFactor = 0.95;
-		} else {
-			discountFactor = 0.98;
-		}
+		final double discountFactor = discountFactor();
 		return basePrice() * discountFactor;
+	}
+
+	private double discountFactor() {
+		if (basePrice() > 1000) 
+			return 0.95;
+		return 0.98;
 	}
 
 	private int basePrice() {
 		return _quantity * _itemPrice;
 	}
 	
-
 }
