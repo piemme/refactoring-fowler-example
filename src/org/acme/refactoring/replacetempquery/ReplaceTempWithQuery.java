dbb633d00bd8878c8f7249b7be5c9a7c4cd51c92
plamank <paolomanca@gmail.com>
2015-01-11 12:26:33 +0100

    rimuovere temp dichiarazione

--- src/org/acme/refactoring/replacetempquery/ReplaceTempWithQuery.java
+++ src/org/acme/refactoring/replacetempquery/ReplaceTempWithQuery.java
@@ -3,7 +3,7 @@ package org.acme.refactoring.replacetempquery;
 /**
  * Martin Fowler, "Refactoring", ch. 6 Composing Methods
  * Replace Temp With Query
- * Versione 0.x, replace prima reference di price
+ * Versione 0.x, rimuovere temp dichiarazione
  */
 public class ReplaceTempWithQuery {
 	
@@ -11,14 +11,13 @@ public class ReplaceTempWithQuery {
 	private int _itemPrice = 500;
 
 	public double getPrice() {
-		final int basePrice = basePrice();
 		final double discountFactor;
 		if (basePrice() > 1000) {
 			discountFactor = 0.95;
 		} else {
 			discountFactor = 0.98;
 		}
-		return basePrice * discountFactor;
+		return basePrice() * discountFactor;
 	}
 
 	private int basePrice() {
